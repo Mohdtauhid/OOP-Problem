81afaac908b2f607d2f744c72eb85d3b79662713
Mohdtauhid <mohdtauhid445577@gmail.com>
2021-02-07 11:46:59 +0530

    # problem 2

--- Question_2.java
+++ Question_2.java
@@ -26,57 +26,314 @@ and stitching cost :
 5. TopOpening SnapLock = 50
 */
 
-
 import java.util.*;
 interface Box
 {
-     private double length, height, width, area;
-     private double GSM, Extra_GSM;
-     private int flute [6]={10,,30,20,40,50,20};  //Price 
-     public void pin();
-     public void pasting();
-     public void single_part();
-     public void double_part();
-     
+   public static final int singlePart=1;
+   public static final int doublePart=3;
+   public void getHeight();
+   public void getWidth();
+   public void getLength();
+   public void getArea();
+   public void getCostOfBox(); 
+   public void getFlute();
 }
-class calcuate_cost_of_box
-{   Box B;                         // reference variable
-private double Area;                length  width  hight
-private double total_GSM;           extraGSM + GSM * 100;
-private double Extra_GSM;            For Flute :
-    calcuate_cost_of_box(Box B)
+class Join
+{
+    private  HashMap<String,Integer> m=new HashMap();
+    private String boxName;
+    
+    public Join(String boxName)
     {
-        this.B=B;
+         this.boxName = boxName;
+         m.put("Universal",20);
+         m.put("AllFlapMeeting",30);
+         m.put("HoneyComb",40);
+         m.put("ReverseTuckin",40);
+         m.put("TopOpeningSnapLock",50);
+    }
+    public double pins()
+    {
+        return 10.0; 
+    }
+   public double pasting()
+   {
+       return m.get(boxName);
+   } 
+}
+class Flute
+{
+    public char type;
+    public double gsm;
+    private double extraGsm;
+    private double totalGsm;
+    
+    public HashMap<Character,Integer> m = new HashMap();
+    
+    public Flute(char type,double gsm)
+    {
+        this.type = type;
+        this.gsm  = gsm;
+        this.totalGsm = 0.0;
+        m.put('A',10);
+        m.put('B',30);
+        m.put('C',20);
+        m.put('E',40);
+        m.put('F',50);
+        m.put('G',20);
     }
     
+    public double getTotalGsm()
+    {
+        totalGsm = m.get(type) + this.gsm*100.0;
+        return totalGsm;
+    }
     
+    public int getExtraGsm()
+    {
+        return m.get(type);
+    }
 }
+
 class Universal implements Box
 {
-    
+    private double length, height, width, area,stitchingCost;
+    private String part;
+    private Flute flute;
+    private double manufacturingCost;
+    public Universal(double length,double height,double width, Flute flute,double stitchingCost,String part)
+    {
+        this.length=length;
+        this.height=height;
+        this.width=width;
+        this.flute=flute;
+        this.stitchingCost=stitchingCost;
+        this.part=part;
+    }
+   public void getHeight()
+   {
+      System.out.println("Height of Universal Box= "+height);
+   }
+   public void getWidth()
+   {
+       System.out.println("Width of Universal Box= "+width);
+   }
+   public void getLength()
+   {
+        System.out.println("Length of Universal Box = "+length);
+   }
+   public void getArea()
+   {   
+       area=length*width*height;
+       System.out.println("Area of Universal Box = "+area);
+   }
+   public void getFlute()
+   {
+    System.out.println("Type = "+flute.type+", Gsm = "+flute.gsm+", ExtraGsm = "+flute.m.get(flute.type));      
+   }
+   public void getCostOfBox()
+   { 
+    manufacturingCost = (part=="singlePart") ? area*flute.getTotalGsm()*flute.getExtraGsm()*stitchingCost*Box.singlePart : area*flute.getTotalGsm()*flute.getExtraGsm()*stitchingCost*Box.doublePart; 
+    System.out.println("Manufacturing Cost of Universal Box = "+manufacturingCost);
+   }
 }
+
 class AllFlapMeeting implements Box
 {
-    
+    private double length, height, width, area,stitchingCost;
+    private String part;
+    private Flute flute;
+    private double manufacturingCost;
+    public AllFlapMeeting(double length,double height,double width, Flute flute,double stitchingCost,String part)
+    {
+        this.length=length;
+        this.height=height;
+        this.width=width;
+        this.flute=flute;
+        this.stitchingCost=stitchingCost;
+        this.part=part;
+    }
+   public void getHeight()
+   {
+      System.out.println("Height of AllFlapMeeting Box= "+height);
+   }
+   public void getWidth()
+   {
+       System.out.println("Width of AllFlapMeeting Box= "+width);
+   }
+   public void getLength()
+   {
+        System.out.println("Length of AllFlapMeeting Box = "+length);
+   }
+   public void getArea()
+   {   
+       area=length*width*height;
+       System.out.println("Area of AllFlapMeeting Box = "+area);
+   }
+   public void getFlute()
+   {
+    System.out.println("Type = "+flute.type+", Gsm = "+flute.gsm+", ExtraGsm = "+flute.m.get(flute.type));      
+   }
+   public void getCostOfBox()
+   { 
+    manufacturingCost = (part=="singlePart") ? area*flute.getTotalGsm()*flute.getExtraGsm()*stitchingCost*Box.singlePart : area*flute.getTotalGsm()*flute.getExtraGsm()*stitchingCost*Box.doublePart; 
+    System.out.println("Manufacturing Cost of AllFlapMeeting Box = "+manufacturingCost);
+   }    
 }
 class HoneyComb implements Box
 {
-    
+   private double length, height, width, area,stitchingCost;
+    private String part;
+    private Flute flute;
+    private double manufacturingCost;
+    public HoneyComb(double length,double height,double width, Flute flute,double stitchingCost,String part)
+    {
+        this.length=length;
+        this.height=height;
+        this.width=width;
+        this.flute=flute;
+        this.stitchingCost=stitchingCost;
+        this.part=part;
+    }
+   public void getHeight()
+   {
+      System.out.println("Height of HoneyComb Box= "+height);
+   }
+   public void getWidth()
+   {
+       System.out.println("Width of HoneyComb Box= "+width);
+   }
+   public void getLength()
+   {
+        System.out.println("Length of HoneyComb Box = "+length);
+   }
+   public void getArea()
+   {   
+       area=length*width*height;
+       System.out.println("Area of HoneyComb Box = "+area);
+   }
+   public void getFlute()
+   {
+    System.out.println("Type = "+flute.type+", Gsm = "+flute.gsm+", ExtraGsm = "+flute.m.get(flute.type));      
+   }
+   public void getCostOfBox()
+   { 
+    manufacturingCost = (part=="singlePart") ? area*flute.getTotalGsm()*flute.getExtraGsm()*stitchingCost*Box.singlePart : area*flute.getTotalGsm()*flute.getExtraGsm()*stitchingCost*Box.doublePart; 
+    System.out.println("Manufacturing Cost of HoneyComb Box = "+manufacturingCost);
+   } 
 }
 class ReverseTuckIn implements Box
 {
-    
+    private double length, height, width, area,stitchingCost;
+    private String part;
+    private Flute flute;
+    private double manufacturingCost;
+    public ReverseTuckIn(double length,double height,double width, Flute flute,double stitchingCost,String part)
+    {
+        this.length=length;
+        this.height=height;
+        this.width=width;
+        this.flute=flute;
+        this.stitchingCost=stitchingCost;
+        this.part=part;
+    }
+   public void getHeight()
+   {
+      System.out.println("Height of ReverseTuckIn Box= "+height);
+   }
+   public void getWidth()
+   {
+       System.out.println("Width of ReverseTuckIn Box= "+width);
+   }
+   public void getLength()
+   {
+        System.out.println("Length of ReverseTuckIn Box = "+length);
+   }
+   public void getArea()
+   {   
+       area=length*width*height;
+       System.out.println("Area of ReverseTuckIn Box = "+area);
+   }
+   public void getFlute()
+   {
+    System.out.println("Type = "+flute.type+", Gsm = "+flute.gsm+", ExtraGsm = "+flute.m.get(flute.type));      
+   }
+   public void getCostOfBox()
+   { 
+    manufacturingCost = (part=="singlePart") ? area*flute.getTotalGsm()*flute.getExtraGsm()*stitchingCost*Box.singlePart : area*flute.getTotalGsm()*flute.getExtraGsm()*stitchingCost*Box.doublePart; 
+    System.out.println("Manufacturing Cost of ReverseTuckIn Box = "+manufacturingCost);
+   }
 }
-class TopOpening SnapLock implements Box
+class TopOpeningSnapLock implements Box
 {
-    
+    private double length, height, width, area,stitchingCost;
+    private String part;
+    private Flute flute;
+    private double manufacturingCost;
+    public TopOpeningSnapLock(double length,double height,double width, Flute flute,double stitchingCost,String part)
+    {
+        this.length=length;
+        this.height=height;
+        this.width=width;
+        this.flute=flute;
+        this.stitchingCost=stitchingCost;
+        this.part=part;
+    }
+   public void getHeight()
+   {
+      System.out.println("Height of TopOpeningSnapLock Box= "+height);
+   }
+   public void getWidth()
+   {
+       System.out.println("Width of TopOpeningSnapLock Box= "+width);
+   }
+   public void getLength()
+   {
+        System.out.println("Length of TopOpeningSnapLock Box = "+length);
+   }
+   public void getArea()
+   {   
+       area=length*width*height;
+       System.out.println("Area of TopOpeningSnapLock Box = "+area);
+   }
+   public void getFlute()
+   {
+    System.out.println("Type = "+flute.type+", Gsm = "+flute.gsm+", ExtraGsm = "+flute.m.get(flute.type));      
+   }
+   public void getCostOfBox()
+   { 
+    manufacturingCost = (part=="singlePart") ? area*flute.getTotalGsm()*flute.getExtraGsm()*stitchingCost*Box.singlePart : area*flute.getTotalGsm()*flute.getExtraGsm()*stitchingCost*Box.doublePart; 
+    System.out.println("Manufacturing Cost of TopOpeningSnapLock Box = "+manufacturingCost);
+   }
 }
 
 public class Main
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello World");
+	Flute flute=new Flute('A',11);
+	Join join =new Join("Universal");
+    double stitchingCost=join.pasting();
+    Box B1 = new Universal(11.2,12.2,13.3,flute,stitchingCost,"doublePart");
+    B1.getLength();
+    B1.getWidth();
+    B1.getHeight();
+    B1.getArea();
+    B1.getFlute();
+    B1.getCostOfBox();
+    
+    
+    Flute flute1=new Flute('G',12);
+	Join join1 =new Join("TopOpeningSnapLock");
+    double stitchingCost1=join.pasting();
+    Box B2 = new TopOpeningSnapLock(10.2,11.2,12.3,flute1,stitchingCost1,"singlePart");
+    B2.getLength();
+    B2.getWidth();
+    B2.getHeight();
+    B2.getArea();
+    B2.getFlute();
+    B2.getCostOfBox();
+    
 	}
 }
 
